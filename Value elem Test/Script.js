9422f01dc0d889217005110a3b089ceb430dd456
DeblinaGhosh1 <85423940+Samannaway@users.noreply.github.com>
2021-07-25 07:05:35 +0000

    Initial commit

---
+++ Value elem Test/Script.js
@@ -0,0 +1,12 @@
+const VAlTest = ()=>{
+    let getData = document.getElementById("Textbox").value
+
+    if (getData == "Deblina Ghosh") {
+        document.getElementById("textDisplay1").innerHTML = "MOM YOU ARE AWESOME"
+    }else if (getData == "Sudipta Ghosh") {
+        document.getElementById("textDisplay1").innerHTML = "DAD YOU ARE MONKEY"
+    }else if (getData == "Samannaway Ghosh") {
+        document.getElementById("textDisplay1").innerHTML = "HE IS A GENIUS"
+    }
+}
+
